9b3cdc9967f124691336c451e7dc445ea0c76267
juanmcost <jmcostanzo610@gmail.com>
2025-10-22 19:28:16 -0300

    add more middlewares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
 import routes from './routes/index.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
@@ -9,9 +11,20 @@ dotenv.config();
 
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: "Too many requests from this IP, please try again later."
+});
+
+
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+}));
+app.use(limiter);
 app.use(morgan('dev'));
+app.use(helmet());
 
 app.use('/api', routes);
 
