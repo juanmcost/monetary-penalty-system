b2102b087d00f2a05402b0d822fd1a6018f6ac48
juanmcost <jmcostanzo610@gmail.com>
2025-10-29 19:28:32 -0300

    remove console.log

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,7 +1,6 @@
 import type { Request, Response, NextFunction } from 'express';
 
 export function errorHandler(err: any, _req: Request, res: Response, _next: NextFunction) {
-  console.error('THERES AN ERROR', err);
   res.status(err.status || 500).json({
     error: err.message || 'Internal Server Error'
   });
