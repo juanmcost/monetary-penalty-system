0dfa63ef7835565af4d5884c782c8347306dffc7
juanmcost <jmcostanzo610@gmail.com>
2025-11-03 19:46:17 -0300

    test CORS

--- backend/src/middleware/CORS.ts
+++ backend/src/middleware/CORS.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 export default cors({
-    origin: process.env.FRONTEND_URL,
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
